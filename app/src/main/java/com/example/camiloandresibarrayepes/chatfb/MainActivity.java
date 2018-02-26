e6f3995c736c6644a1d9876fb6e2922f7330ca0b
camilo ibarra <camilo.ibarray@gmail.com>
2018-02-25 22:39:22 -0500

    Envio y guardado de datos en firebase

--- app/src/main/java/com/example/camiloandresibarrayepes/chatfb/MainActivity.java
+++ app/src/main/java/com/example/camiloandresibarrayepes/chatfb/MainActivity.java
@@ -10,6 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
     private AdapterMensajes adapter;
 
+    //Envio datos a FIREBASE
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         txtMensaje = (EditText)findViewById(R.id.txtMensaje);
         btnEnviar = (Button)findViewById(R.id.btnEnviar);
 
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("chat");//Sala de chat (nombre donde se guardan mensajes)
+
         adapter = new AdapterMensajes(this);
         LinearLayoutManager l = new LinearLayoutManager(this);
         rvMensajes.setLayoutManager(l);
@@ -41,8 +54,56 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                adapter.addMensaje(new Mensaje(txtMensaje.getText().toString(), nombre.getText().toString(), "", "1", "00:00" ));
+                //Insertar mensaje enla base de datos
+                databaseReference.push().setValue(new Mensaje(txtMensaje.getText().toString(), nombre.getText().toString(), "", "1", "00:00" ));
+                //adapter.addMensaje(new Mensaje(txtMensaje.getText().toString(), nombre.getText().toString(), "", "1", "00:00" ));
+                txtMensaje.setText("");
             }
         });
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+                setScrollBar();
+
+            }
+        });
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            //Este metodo permite
+            //Cuando se agrege un dato a la base de datos, lo agrega a la vista
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Mensaje m = dataSnapshot.getValue(Mensaje.class);
+                adapter.addMensaje(m);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    //permite que los mensajes de desplacen hacia arriba a medida que aparecen
+    private void setScrollBar(){
+        rvMensajes.scrollToPosition(adapter.getItemCount()-1);
     }
 }
